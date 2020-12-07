7ff358f9722cc722ec0d33c1eab99449f3aecd93
Javier Martinez <javijog@gmail.com>
2020-12-07 01:52:00 +0100

    Atasco Ejer4

--- src/main/java/com/example/Reto1/API/OrdersProductsController.java
+++ src/main/java/com/example/Reto1/API/OrdersProductsController.java
@@ -7,7 +7,6 @@ import java.util.List;
 import com.example.Reto1.Model.OrderProduct;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
