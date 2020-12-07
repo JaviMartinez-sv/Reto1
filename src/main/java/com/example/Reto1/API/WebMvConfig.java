2105376540167edf1e88dd71b3263fdda34872cd
Javier Martinez <javijog@gmail.com>
2020-12-07 01:58:50 +0100

    Inicio Ejercicio 8

---
+++ src/main/java/com/example/Reto1/API/WebMvConfig.java
@@ -0,0 +1,42 @@
+package com.example.Reto1.API;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+@Configuration
+@ComponentScan("com.example.Reto1.API")
+public class WebMvConfig implements WebMvcConfigurer{
+    @Bean
+    public CookieLocaleResolver localeResolver() {
+        return new CookieLocaleResolver();
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeInterceptor() {
+        LocaleChangeInterceptor localeInterceptor = new LocaleChangeInterceptor();
+        localeInterceptor.setParamName("lang");
+        return localeInterceptor;
+    }
+
+    @Bean(name = "messageSource")
+    public MessageSource getMessageResource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasenames("classpath:i18n/messages", "classpath:i18n/lang");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeInterceptor());
+
+    }
+    // Creacion de cookie para guardar ultimo idioma
+}
